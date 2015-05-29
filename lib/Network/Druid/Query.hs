6cb9e5add84811e2611f1f5b7c39a90e5350d3d4
Christian Marie <christian@ponies.io>
2015-05-29 17:03:41 +1000

    Initial commit

---
+++ lib/Network/Druid/Query.hs
@@ -0,0 +1,179 @@
+--
+-- Copyright © 2013-2015 Anchor Systems, Pty Ltd and Others
+--
+-- The code in this file, and the program it is a part of, is
+-- made available to you by its authors as open source software:
+-- you can redistribute it and/or modify it under the terms of
+-- the 3-clause BSD licence.
+--
+
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+
+module Network.Druid.Query
+(
+    -- * Query ADT
+    Query(..),
+    Threshold(..),
+    DataSource(..),
+    Granularity(..),
+    Filter(..),
+    Dimension(..),
+    Aggregation(..),
+    PostAggregation(..),
+    Interval(..),
+    Metric(..),
+
+) where
+
+import Control.Applicative
+import Data.Aeson
+import Data.Maybe
+import Data.Monoid
+import Data.Text
+
+-- | Druid has numerous query types for various use cases. Queries are composed
+-- of various JSON properties and Druid has different types of queries for
+-- different use cases.
+data Query
+    -- | These types of queries take a timeseries query object and return an
+    -- array of JSON objects where each object represents a value asked for by
+    -- the timeseries query.
+    = QueryTimeSeries
+        { _queryDataSource       :: DataSource
+        , _queryGranularity      :: Granularity
+        , _queryFilter           :: Maybe Filter
+        , _queryAggregations     :: [Aggregation]
+        , _queryPostAggregations :: [PostAggregation]
+        , _queryIntervals        :: [Interval]
+        }
+    -- | TopN queries return a sorted set of results for the values in a given
+    -- dimension according to some criteria. Conceptually, they can be thought
+    -- of as an approximate GroupByQuery over a single dimension with an
+    -- Ordering spec. TopNs are much faster and resource efficient than
+    -- GroupBys for this use case. These types of queries take a topN query
+    -- object and return an array of JSON objects where each object represents
+    -- a value asked for by the topN query.
+    --
+    -- TopNs are approximate in that each node will rank their top K results
+    -- and only return those top K results to the broker. K, by default in
+    -- Druid, is max(1000, threshold). In practice, this means that if you ask
+    -- for the top 1000 items ordered, the correctness of the first ~900 items
+    -- will be 100%, and the ordering of the results after that is not
+    -- guaranteed. TopNs can be made more accurate by increasing the threshold.
+    | QueryTopN
+        { _queryDataSource       :: DataSource
+        , _queryGranularity      :: Granularity
+        , _queryFilter           :: Maybe Filter
+        , _queryAggregations     :: [Aggregation]
+        , _queryPostAggregations :: [PostAggregation]
+        , _queryIntervals        :: [Interval]
+        , _queryDimensions       :: [Dimension]
+        , _queryThreshold        :: Threshold
+        , _queryMetric           :: Metric
+        }
+    -- | These types of queries take a groupBy query object and return an array
+    -- of JSON objects where each object represents a grouping asked for by the
+    -- query. Note: If you only want to do straight aggregates for some time
+    -- range, we highly recommend using TimeseriesQueries instead. The
+    -- performance will be substantially better. If you want to do an ordered
+    -- groupBy over a single dimension, please look at TopN queries. The
+    -- performance for that use case is also substantially better.
+    | QueryGroupBy
+
+data Threshold
+
+-- | A data source is the Druid equivalent of a database table. However, a
+-- query can also masquerade as a data source, providing subquery-like
+-- functionality. Query data sources are currently supported only by GroupBy
+-- queries.
+data DataSource = DataSource
+
+
+-- | The granularity field determines how data gets bucketed across the time
+-- dimension, or how it gets aggregated by hour, day, minute, etc.
+--
+-- It can be specified either as a string for simple granularities or as an
+-- object for arbitrary granularities.
+data Granularity
+    = GranularityAll
+    | GranularityNone
+    | GranularityMinute
+    | GranularityFifteenMinute
+    | GranularityThirtyMinute
+    | GranularityHour
+    | GranularityDay
+
+-- | A filter is a JSON object indicating which rows of data should be included
+-- in the computation for a query. It’s essentially the equivalent of the WHERE
+-- clause in SQL. Druid supports the following types of filters.
+data Filter
+    -- | The simplest filter is a selector filter. The selector filter will
+    -- match a specific dimension with a specific value. Selector filters can
+    -- be used as the base filters for more complex Boolean expressions of
+    -- filters.
+    = FilterSelector
+        { _selectorDimension :: Text
+        , _selectorValue     :: Text
+        }
+    | FilterRegularExpression
+        { _selectorDimension :: Text
+        , _selectorPattern   :: Text }
+    | FilterJS
+        { _selectorDimension :: Text
+        , _selectorFunction  :: Text }
+    | FilterAnd { _selectorFields :: [Filter] }
+    | FilterOr  { _selectorFields :: [Filter] }
+    | FilterNot { _selectorField  :: Filter }
+
+data Dimension
+data Aggregation
+data PostAggregation
+data Interval
+data Metric
+
+-- * Instances
+
+instance ToJSON Query where
+    toJSON QueryTimeSeries{..} = object $
+        [ "granularity" .= toJSON _queryGranularity ]
+        <> fmap (("filter" .=) . toJSON) (maybeToList _queryFilter)
+
+instance ToJSON Granularity where
+    toJSON GranularityAll           = "all"
+    toJSON GranularityNone          = "none"
+    toJSON GranularityMinute        = "minute"
+    toJSON GranularityFifteenMinute = "fifteen_minute"
+    toJSON GranularityThirtyMinute  = "thirty_minute"
+    toJSON GranularityHour          = "hour"
+    toJSON GranularityDay           = "day"
+
+instance ToJSON Filter where
+    toJSON FilterSelector{..} = object $
+        [ "type" .= String "selector"
+        , "dimension" .= _selectorDimension
+        , "value" .= _selectorValue
+        ]
+    toJSON FilterRegularExpression{..} = object $
+        [ "type" .= String "regex"
+        , "dimension" .= _selectorDimension
+        , "pattern" .= _selectorPattern
+        ]
+    toJSON FilterJS{..} = object $
+        [ "type" .= String "javascript"
+        , "dimension" .= _selectorDimension
+        , "function" .=  _selectorFunction
+        ]
+    toJSON FilterAnd{..} = object $
+        [ "type" .= String "and"
+        , "fields" .=  toJSON _selectorFields
+        ]
+    toJSON FilterOr{..} = object $
+        [ "type" .= String "or"
+        , "fields" .=  toJSON _selectorFields
+        ]
+    toJSON FilterNot{..} = object $
+        [ "type" .= String "not"
+        , "field" .=  toJSON _selectorField
+        ]
---
+++ tests/unit.hs
@@ -0,0 +1,264 @@
+--
+-- Copyright © 2013-2015 Anchor Systems, Pty Ltd and Others
+--
+-- The code in this file, and the program it is a part of, is
+-- made available to you by its authors as open source software:
+-- you can redistribute it and/or modify it under the terms of
+-- the 3-clause BSD licence.
+--
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Main where
+
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+import Data.Aeson
+import Data.Aeson.QQ
+
+import Network.Druid.Query
+
+main :: IO ()
+main = hspec suite
+
+-- * Example queries
+
+timeSeriesQueryV :: Value
+timeSeriesQueryV = [aesonQQ|
+{
+   "granularity" : "day",
+   "filter" : {
+      "type" : "and",
+      "fields" : [
+         {
+            "value" : "sample_value1",
+            "type" : "selector",
+            "dimension" : "sample_dimension1"
+         },
+         {
+            "type" : "or",
+            "fields" : [
+               {
+                  "value" : "sample_value2",
+                  "type" : "selector",
+                  "dimension" : "sample_dimension2"
+               },
+               {
+                  "value" : "sample_value3",
+                  "type" : "selector",
+                  "dimension" : "sample_dimension3"
+               }
+            ]
+         }
+      ]
+   },
+   "dataSource" : "sample_datasource",
+   "postAggregations" : [
+      {
+         "name" : "sample_divide",
+         "type" : "arithmetic",
+         "fields" : [
+            {
+               "name" : "sample_name1",
+               "type" : "fieldAccess",
+               "fieldName" : "sample_fieldName1"
+            },
+            {
+               "name" : "sample_name2",
+               "fieldName" : "sample_fieldName2",
+               "type" : "fieldAccess"
+            }
+         ],
+         "fn" : "/"
+      }
+   ],
+   "aggregations" : [
+      {
+         "name" : "sample_name1",
+         "type" : "longSum",
+         "fieldName" : "sample_fieldName1"
+      },
+      {
+         "name" : "sample_name2",
+         "type" : "doubleSum",
+         "fieldName" : "sample_fieldName2"
+      }
+   ],
+   "intervals" : [
+      "2012-01-01T00:00:00.000/2012-01-03T00:00:00.000"
+   ],
+   "queryType" : "timeseries"
+}
+|]
+
+timeSeriesQueryQ :: Query
+timeSeriesQueryQ =
+    QueryTimeSeries
+        DataSource
+        GranularityDay
+        (Just $ FilterAnd
+            [ FilterSelector "sample_dimension1" "sample_value1"
+            , FilterOr
+                [ FilterSelector "sample_dimension2" "sample_value2"
+                , FilterSelector "sample_dimension3" "sample_value3"
+                ]
+            ])
+        []
+        []
+        []
+
+
+topNQueryV :: Value
+topNQueryV = [aesonQQ|
+{
+  "queryType": "topN",
+  "dataSource": "sample_data",
+  "dimension": "sample_dim",
+  "threshold": 5,
+  "metric": "count",
+  "granularity": "all",
+  "filter": {
+    "type": "and",
+    "fields": [
+      {
+        "type": "selector",
+        "dimension": "dim1",
+        "value": "some_value"
+      },
+      {
+        "type": "selector",
+        "dimension": "dim2",
+        "value": "some_other_val"
+      }
+    ]
+  },
+  "aggregations": [
+    {
+      "type": "longSum",
+      "name": "count",
+      "fieldName": "count"
+    },
+    {
+      "type": "doubleSum",
+      "name": "some_metric",
+      "fieldName": "some_metric"
+    }
+  ],
+  "postAggregations": [
+    {
+      "type": "arithmetic",
+      "name": "sample_divide",
+      "fn": "/",
+      "fields": [
+        {
+          "type": "fieldAccess",
+          "name": "some_metric",
+          "fieldName": "some_metric"
+        },
+        {
+          "type": "fieldAccess",
+          "name": "count",
+          "fieldName": "count"
+        }
+      ]
+    }
+  ],
+  "intervals": [
+    "2013-08-31T00:00:00.000/2013-09-03T00:00:00.000"
+  ]
+}
+|]
+
+groupByQueryV :: Value
+groupByQueryV = [aesonQQ|
+{
+   "filter" : {
+      "fields" : [
+         {
+            "dimension" : "carrier",
+            "type" : "selector",
+            "value" : "AT&T"
+         },
+         {
+            "type" : "or",
+            "fields" : [
+               {
+                  "dimension" : "make",
+                  "value" : "Apple",
+                  "type" : "selector"
+               },
+               {
+                  "dimension" : "make",
+                  "type" : "selector",
+                  "value" : "Samsung"
+               }
+            ]
+         }
+      ],
+      "type" : "and"
+   },
+   "queryType" : "groupBy",
+   "dimensions" : [
+      "country",
+      "device"
+   ],
+   "aggregations" : [
+      {
+         "fieldName" : "user_count",
+         "type" : "longSum",
+         "name" : "total_usage"
+      },
+      {
+         "name" : "data_transfer",
+         "type" : "doubleSum",
+         "fieldName" : "data_transfer"
+      }
+   ],
+   "dataSource" : "sample_datasource",
+   "limitSpec" : {
+      "limit" : 5000,
+      "columns" : [
+         "country",
+         "data_transfer"
+      ],
+      "type" : "default"
+   },
+   "intervals" : [
+      "2012-01-01T00:00:00.000/2012-01-03T00:00:00.000"
+   ],
+   "having" : {
+      "value" : 100,
+      "type" : "greaterThan",
+      "aggregation" : "total_usage"
+   },
+   "postAggregations" : [
+      {
+         "name" : "avg_usage",
+         "type" : "arithmetic",
+         "fields" : [
+            {
+               "type" : "fieldAccess",
+               "fieldName" : "data_transfer"
+            },
+            {
+               "fieldName" : "total_usage",
+               "type" : "fieldAccess"
+            }
+         ],
+         "fn" : "/"
+      }
+   ],
+   "granularity" : "day"
+}
+|]
+
+suite :: Spec
+suite = 
+    describe "ToJSON for Query" $ do
+        it "has correct output for known TimeSeries" $ do
+            toJSON timeSeriesQueryQ `shouldBe` timeSeriesQueryV
+        it "has correct output for known TopN"  pending
+        it "has correct output for known GroupBy"  pending
+        it "has correct output for many combinations"  pending
